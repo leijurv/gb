85812fcd88c1602947dd9b09f3105dd416edf34a
Babbaj <babbaj45@gmail.com>
2021-03-06 00:11:56 -0500

    Fix subdirectories that only have more subdirectories not being listed

--- history/dir.go
+++ history/dir.go
@@ -19,7 +19,7 @@ func DirHistory(basePath string) {
 	}
 	log.Println("Fetching history of", basePath)
 	log.Println("This will only work on directories. For files, use \"history\" instead of \"ls\".")
-	rows, err := db.DB.Query(`SELECT path, COUNT(*) AS num_revisions, MIN(start) AS first_backup, MAX(fs_modified) AS max_fs_modified, MIN(COALESCE(end, 0)) AS min_end FROM files WHERE path GLOB ? AND path NOT GLOB ? GROUP BY path`, basePath+"*", basePath+"*/*/*")
+	rows, err := db.DB.Query(`SELECT path, COUNT(*) AS num_revisions, MIN(start) AS first_backup, MAX(fs_modified) AS max_fs_modified, MIN(COALESCE(end, 0)) AS min_end FROM files WHERE path GLOB ? GROUP BY path`, basePath+"*")
 	if err != nil {
 		panic(err)
 	}
@@ -57,7 +57,8 @@ func DirHistory(basePath string) {
 			}
 			log.Println(line)
 		} else if len(dir) != 0 {
-			subDirs[basePath+dir] = struct{}{}
+			truncated := dir[:strings.Index(dir, "/")]
+			subDirs[basePath+truncated] = struct{}{}
 		}
 	}
 	log.Println("Directories:")

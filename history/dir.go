4f4e82414e783c09a3825a3e2afe94dde57e6cfc
Babbaj <babbaj45@gmail.com>
2021-03-05 22:55:37 -0500

    Add subdirectories to output of ls

--- history/dir.go
+++ history/dir.go
@@ -1,32 +1,32 @@
 package history
 
 import (
+	"github.com/leijurv/gb/db"
 	"log"
 	"path/filepath"
 	"strings"
 	"time"
-
-	"github.com/leijurv/gb/db"
 )
 
-func DirHistory(path string) {
+func DirHistory(basePath string) {
 	var err error
-	path, err = filepath.Abs(path)
+	basePath, err = filepath.Abs(basePath)
 	if err != nil {
 		panic(err)
 	}
-	if !strings.HasSuffix(path, "/") {
-		path += "/"
+	if !strings.HasSuffix(basePath, "/") {
+		basePath += "/"
 	}
-	log.Println("Fetching history of", path)
+	log.Println("Fetching history of", basePath)
 	log.Println("This will only work on directories. For files, use \"history\" instead of \"ls\".")
-	rows, err := db.DB.Query(`SELECT path, COUNT(*) AS num_revisions, MIN(start) AS first_backup, MAX(fs_modified) AS max_fs_modified, MIN(COALESCE(end, 0)) AS min_end FROM files WHERE path GLOB ? AND path NOT GLOB ? GROUP BY path`, path+"*", path+"*/*")
+	rows, err := db.DB.Query(`SELECT path, COUNT(*) AS num_revisions, MIN(start) AS first_backup, MAX(fs_modified) AS max_fs_modified, MIN(COALESCE(end, 0)) AS min_end FROM files WHERE path GLOB ? AND path NOT GLOB ? GROUP BY path`, basePath+"*", basePath+"*/*/*")
 	if err != nil {
 		panic(err)
 	}
 	defer rows.Close()
 	log.Println()
 	log.Println("Path: Number of revisions, timestamp of first backup, most recent backed up filesystem last modified time, currently exists")
+	subDirs := map[string]struct{}{}
 	for rows.Next() {
 		var path string
 		var numRevs int64
@@ -38,18 +38,31 @@ func DirHistory(path string) {
 			panic(err)
 		}
 		// TODO use % formatting :(
-		line := path
-		line += ": "
-		line += time.Unix(firstBackup, 0).Format(time.RFC3339)
-		line += ", "
-		line += time.Unix(maxFsModified, 0).Format(time.RFC3339)
-		line += ", "
-		if minEnd == 0 {
-			line += "true"
-		} else {
-			line += "false"
+		rel, err := filepath.Rel(basePath, path)
+		if err != nil {
+			panic(err)
 		}
-		log.Println(line)
+		dir, f := filepath.Split(rel)
+		if len(dir) == 0 && len(f) != 0 {
+			line := path
+			line += ": "
+			line += time.Unix(firstBackup, 0).Format(time.RFC3339)
+			line += ", "
+			line += time.Unix(maxFsModified, 0).Format(time.RFC3339)
+			line += ", "
+			if minEnd == 0 {
+				line += "true"
+			} else {
+				line += "false"
+			}
+			log.Println(line)
+		} else if len(dir) != 0 {
+			subDirs[basePath+dir] = struct{}{}
+		}
+	}
+	log.Println("Directories:")
+	for dir, _ := range subDirs {
+		log.Println(dir)
 	}
 	err = rows.Err()
 	if err != nil {

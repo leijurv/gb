44afa319f1686b298a30ffed394d69152d159a94
Leijurv <leijurv@gmail.com>
2021-10-25 12:19:31 -0700

    multithread replication

--- replicate/replicate.go
+++ replicate/replicate.go
@@ -3,12 +3,15 @@ package replicate
 import (
 	"io"
 	"log"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/leijurv/gb/db"
 	"github.com/leijurv/gb/paranoia"
 	storagepkg "github.com/leijurv/gb/storage"
 	"github.com/leijurv/gb/utils"
+	"github.com/leijurv/gb/storage_base"
 )
 
 func ReplicateBlobs(label string) {
@@ -23,7 +26,7 @@ func ReplicateBlobs(label string) {
 	log.Println("This will just go through the blobs in", storage)
 	log.Println("It won't go through what's in the database, so make sure that that's all good (such as with `gb paranoia storage` lol)")
 	toReplicate := storage.ListBlobs()
-	var sz int64
+	sz := new(int64)
 	for _, dst := range storagepkg.GetAll() {
 		if dst == storage {
 			continue
@@ -32,21 +35,36 @@ func ReplicateBlobs(label string) {
 		for _, inDst := range dst.ListBlobs() {
 			alreadyHere[utils.SliceToArr(inDst.BlobID)] = struct{}{}
 		}
+		todo := make(chan storage_base.UploadedBlob)
+		var wg sync.WaitGroup
+		for i := 0; i < 8; i++ {
+			j := i
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for blob := range todo {
+					log.Println("Copy", blob, "from", storage, "to", dst)
+					log.Println("Done", atomic.LoadInt64(sz), "bytes, thread", j)
+					reader := paranoia.DownloadEntireBlob(blob.BlobID, storage)
+					out := dst.BeginBlobUpload(blob.BlobID)
+					rd := io.TeeReader(reader, out.Writer())
+					bytes := paranoia.BlobReaderParanoia(rd, blob.BlobID, storage)
+					atomic.AddInt64(sz, bytes)
+					completed := out.End()
+					_, err := db.DB.Exec("INSERT INTO blob_storage (blob_id, storage_id, path, checksum, timestamp) VALUES (?, ?, ?, ?, ?)", blob.BlobID, completed.StorageID, completed.Path, completed.Checksum, time.Now().Unix())
+					if err != nil {
+						panic(err)
+					}
+				}
+			}()
+		}
 		for _, blob := range toReplicate {
 			if _, ok := alreadyHere[utils.SliceToArr(blob.BlobID)]; ok {
 				continue
 			}
-			log.Println("Copy", blob, "from", storage, "to", dst)
-			log.Println("Done", sz, "bytes")
-			reader := paranoia.DownloadEntireBlob(blob.BlobID, storage)
-			out := dst.BeginBlobUpload(blob.BlobID)
-			rd := io.TeeReader(reader, out.Writer())
-			sz += paranoia.BlobReaderParanoia(rd, blob.BlobID, storage)
-			completed := out.End()
-			_, err := db.DB.Exec("INSERT INTO blob_storage (blob_id, storage_id, path, checksum, timestamp) VALUES (?, ?, ?, ?, ?)", blob.BlobID, completed.StorageID, completed.Path, completed.Checksum, time.Now().Unix())
-			if err != nil {
-				panic(err)
-			}
+			todo <- blob
 		}
+		close(todo)
+		wg.Wait()
 	}
 }

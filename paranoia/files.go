2920bdbcd79a63dad4a5a7b20525d7610a04ffa6
Leijurv <leijurv@gmail.com>
2025-12-13 22:49:48 -0800

    parallel files paranoia

--- paranoia/files.go
+++ paranoia/files.go
@@ -12,7 +12,9 @@ import (
 
 func TestAllFiles() {
 	hashes := make(chan []byte, 100)
-	for worker := 0; worker < 16; worker++ {
+	success := make(chan bool)
+	nWorkers := 16
+	for worker := 0; worker < nWorkers; worker++ {
 		go func() {
 			tx, err := db.DB.Begin()
 			if err != nil {
@@ -24,6 +26,7 @@ func TestAllFiles() {
 					panic(err)
 				}
 			}()
+			didISucceed := true
 			for hash := range hashes {
 				log.Println("Testing fetching hash", hex.EncodeToString(hash), "which is the contents of") //, path)
 				reader := download.Cat(hash, tx)
@@ -32,31 +35,43 @@ func TestAllFiles() {
 				realHash, realSize := h.HashAndSize()
 				log.Println("Size is", realSize, "and hash is", hex.EncodeToString(realHash))
 				if !bytes.Equal(realHash, hash) {
-					panic(":(")
+					log.Println("WRONG", hex.EncodeToString(hash), hex.EncodeToString(realHash), realSize)
+					didISucceed = false
+				} else {
+					log.Println("Hash is equal!")
 				}
-				log.Println("Hash is equal!")
 			}
+			success <- didISucceed
 		}()
 	}
-	// TODO some other ordering idk? this is just the most recent files you uploaded, which is reasonable i think?
-	//rows, err := tx.Query(`SELECT d.hash FROM (SELECT DISTINCT hash FROM files WHERE end IS NULL) d INNER JOIN sizes ON sizes.hash = d.hash WHERE sizes.size < 100000000 ORDER BY sizes.hash`)
-	// SELECT DISTINCT hash, path FROM (SELECT files.*, sizes.size FROM files INNER JOIN sizes ON files.hash = sizes.hash) WHERE size < 10000000 ORDER BY start DESC
-	rows, err := db.DB.Query(`SELECT hash FROM blob_entries WHERE compression_alg = "lepton"`)
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var hash []byte
-		//var path string
-		err := rows.Scan(&hash) //, &path)
+	go func() {
+		// TODO some other ordering idk? this is just the most recent files you uploaded, which is reasonable i think?
+		//rows, err := tx.Query(`SELECT d.hash FROM (SELECT DISTINCT hash FROM files WHERE end IS NULL) d INNER JOIN sizes ON sizes.hash = d.hash WHERE sizes.size < 100000000 ORDER BY sizes.hash`)
+		// SELECT DISTINCT hash, path FROM (SELECT files.*, sizes.size FROM files INNER JOIN sizes ON files.hash = sizes.hash) WHERE size < 10000000 ORDER BY start DESC
+		rows, err := db.DB.Query(`SELECT hash FROM blob_entries WHERE compression_alg = "lepton" GROUP BY hash ORDER BY hash ASC`)
 		if err != nil {
 			panic(err)
 		}
-		hashes <- hash
+		defer rows.Close()
+		for rows.Next() {
+			var hash []byte
+			//var path string
+			err := rows.Scan(&hash) //, &path)
+			if err != nil {
+				panic(err)
+			}
+			hashes <- hash
+		}
+		err = rows.Err()
+		if err != nil {
+			panic(err)
+		}
+		close(hashes)
+	}()
+	for worker := 0; worker < nWorkers; worker++ {
+		if !<- success {
+			panic("fail")
+		}
 	}
-	err = rows.Err()
-	if err != nil {
-		panic(err)
-	}
+	log.Println("Success")
 }

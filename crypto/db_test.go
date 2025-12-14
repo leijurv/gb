e3fb9d37fd65864f2d368f209f6466718c3c571e
Leijurv <leijurv@gmail.com>
2025-12-14 00:46:22 -0800

    more crypto tests

--- crypto/db_test.go
+++ crypto/db_test.go
@@ -46,3 +46,77 @@ func TestV2Enc(t *testing.T) {
 		t.Error("unequal")
 	}
 }
+
+func TestLegacyDecryptCorruptedData(t *testing.T) {
+	data := RandBytes(100)
+	key := RandBytes(16)
+	enc := LegacyEncryptDatabase(data, key)
+
+	corrupted := make([]byte, len(enc))
+	copy(corrupted, enc)
+	corrupted[len(corrupted)-1] ^= 0xff
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("expected panic on corrupted GCM data")
+		}
+	}()
+	LegacyDecryptDatabase(corrupted, key)
+}
+
+func TestV2DecryptCorruptedMAC(t *testing.T) {
+	data := RandBytes(100)
+	key := RandBytes(16)
+	rawDB := utils.NewSHA256HasherSizer()
+	var buf bytes.Buffer
+	out := EncryptDatabaseV2(&buf, key)
+	out.Write(data)
+	rawDB.Write(data)
+	out.Write(ComputeMAC(rawDB.Hash(), key))
+	enc := buf.Bytes()
+
+	corrupted := make([]byte, len(enc))
+	copy(corrupted, enc)
+	corrupted[len(corrupted)-1] ^= 0xff
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("expected panic on corrupted MAC")
+		}
+	}()
+	DecryptDatabaseV2(corrupted, key)
+}
+
+func TestV2DecryptWrongKey(t *testing.T) {
+	data := RandBytes(100)
+	key := RandBytes(16)
+	wrongKey := RandBytes(16)
+	rawDB := utils.NewSHA256HasherSizer()
+	var buf bytes.Buffer
+	out := EncryptDatabaseV2(&buf, key)
+	out.Write(data)
+	rawDB.Write(data)
+	out.Write(ComputeMAC(rawDB.Hash(), key))
+	enc := buf.Bytes()
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("expected panic on wrong key")
+		}
+	}()
+	DecryptDatabaseV2(enc, wrongKey)
+}
+
+func TestLegacyDecryptWrongKey(t *testing.T) {
+	data := RandBytes(100)
+	key := RandBytes(16)
+	wrongKey := RandBytes(16)
+	enc := LegacyEncryptDatabase(data, key)
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("expected panic on wrong key")
+		}
+	}()
+	LegacyDecryptDatabase(enc, wrongKey)
+}
